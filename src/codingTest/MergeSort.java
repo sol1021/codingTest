dd137168ebf10ce2757e8ad757f6d5c7f7240bc4
sol <soool1021@gmail.com>
2022-10-05 22:17:09 +0900

    머지소트 수정

--- src/codingTest/MergeSort.java
+++ src/codingTest/MergeSort.java
@@ -1,55 +1,67 @@
 package codingTest;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+//BJ1427
 public class MergeSort {
-    static int[] sorted = new int[8];
-    public static void merge(int a[], int m, int middle, int n) {
-        int i = m;             // 첫 번째 부분집합의 시작 위치 설정
-        int j = middle+1;     // 두 번째 부분집합의 시작 위치 설정
-        int k = m;             // 배열 sorted에 정렬된 원소를 저장할 위치 설정
-        
-        while(i<=middle && j<=n) {
-            if(a[i]<=a[j]) {
-                sorted[k] = a[i];
-                i++;
-            }else {
-                sorted[k] = a[j];
-                j++;
-            }
-            k++;
-        }
-        if(i>middle) {
-            for(int t=j;t<=n;t++,k++) {
-                sorted[k] = a[t];
-            }
-        }else {
-            for(int t=i;t<=middle;t++,k++) {
-                sorted[k] = a[t];
-            }
-        }
-        
-        for(int t=m;t<=n;t++) {
-            a[t] = sorted[t];
-        }
-    }
-        
-    
-    public static void mergeSort(int a[], int m, int n) {
-        int middle;
-        if(m<n) {
-            middle = (m+n)/2;
-            mergeSort(a, m, middle);    // 앞 부분에 대한 분할 작업 수행
-            mergeSort(a, middle+1, n);    // 뒷 부분에 대한 분할 작업 수행
-            merge(a, m, middle, n);        // 부분집합에 대하여 정렬과 병합 작업 수행
-        }
-    }
-    public static void main(String[] args) {
-        int[] list = {5, 3, 2, 6, 4, 7, 8, 1};
-        int size = list.length;
-        mergeSort(list, 0, size-1);
-        for(int i=0; i<size; i++) {
-        	System.out.print(list[i] + " ");
-        }
-      
-    }
+	public static int[] tmp; // 배열의 값을 잠시 복사해둘 공간
+	
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] arr = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+		mergeSort(arr);
+		for(int i =0;i<arr.length;i++) {
+			System.out.print(arr[i]);
+		}
+	}
  
+	public static void mergeSort(int[] arr) {
+		tmp = new int[arr.length];
+		mergeSort(arr, 0, arr.length - 1);
+	}
+ 
+	private static void mergeSort(int[] arr, int start, int end) {
+		// 원소가 2개 이상이라면
+		if(start < end) {
+			// 중앙 값을 구하고
+			int mid = (start + end) / 2;
+			// 중앙을 기준으로 2개의 그룹으로 분할
+			mergeSort(arr, start, mid);
+			mergeSort(arr, mid+1, end);
+			// 정렬 후 병합
+			merge(arr, start, mid, end);
+		}
+	}
+ 
+	private static void merge(int[] arr, int start, int mid, int end) {
+		// 배열 복사
+		tmp = arr.clone();
+		
+		/*
+		 * part1: 왼쪽 그룹의 시작 인덱스
+		 * part2: 오른쪽 그룹의 시작 인덱스
+		 * index: 정렬된 값을 병합된 배열의 어떤 위치에 넣어야하는 지
+		 */
+		int part1 = start;
+		int part2 = mid + 1;
+		int index = start;
+		
+		// 두 그룹중 한쪽의 원소가 오링날때까지 반복 (정렬 & 합병)
+		while(part1 <= mid && part2 <= end) {
+			if(tmp[part1] >= tmp[part2]) {
+				arr[index++] = tmp[part1++];
+			} else {
+				arr[index++] = tmp[part2++];
+			}
+		}
+		
+		// 왼쪽 그룹의 원소가 남는 경우 처리
+		// (오른쪽 그룹의 원소가 남는 경우는 따로 처리하지 않아도 된다)
+		for(int i=0; i<=mid-part1; i++) {
+			arr[index+i] = tmp[part1+i];
+		}
+	}
 }
