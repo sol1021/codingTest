99114760972b499e2701efed67f3599e185e1e32
sol <soool1021@gmail.com>
2022-10-20 22:18:58 +0900

    10-20

--- src/codingTest/BJ4948.java
+++ src/codingTest/BJ4948.java
@@ -1,27 +1,31 @@
 package codingTest;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class BJ4948 {
-	public static void main(String[] args)  throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int result =0;
-		for(int i=n;i<n*2;i++) {
-			boolean isPrime = true;
-			if(i==1) continue;
-			for(int j=2;j<i;j++) {
-				if(i%j==0) {
-					isPrime = false;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int n = sc.nextInt();
+			if(n==0) break;
+			
+			int result =0;
+			for(int i=n+1;i<=n*2;i++) {
+				if(n==1) {
+					result=1;
 					break;
 				}
+				boolean isPrime = true;
+				for(int j=2;j<=(int)Math.sqrt(i);j++) {
+					if(i%j==0) {
+						isPrime = false;
+						break;
+					}
+				}
+				if(isPrime) result++;
 			}
-			if(isPrime) result++;
+			System.out.println(result);
 		}
-		System.out.println(result);
+		sc.close();
 	}
 }
